bb8ab523742c5f5b5548425d74b6e0dcf4c56bff
iskenderColak <60299811+iColak@users.noreply.github.com>
2022-02-20 22:47:59 +0300

    Add files via upload

---
+++ LetterB.java
@@ -0,0 +1,25 @@
+package arrays;
+
+public class LetterB {
+    public static void main(String[] args) {
+        String[][] array = new String[7][4];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (i == 0 || i == 3 || i == 6) {
+                    array[i][j] = " * ";
+                } else if (j == 0 || j == 3) {
+                    array[i][j] = " * ";
+                } else {
+                    array[i][j] = "   ";
+                }
+            }
+        }
+
+        for (String[] row : array) {
+            for (String column : row) {
+                System.out.print(column);
+            }
+            System.out.println();
+        }
+    }
+}
